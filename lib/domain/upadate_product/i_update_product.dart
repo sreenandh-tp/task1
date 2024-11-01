01d0b40e09286322764d44f66661f51e5f8c8657
Sreenandh <sreenathtp38@gmail.com>
2024-11-01 12:26:47 +0530

    1/11/2024

--- lib/domain/upadate_product/i_update_product.dart
+++ lib/domain/upadate_product/i_update_product.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:sreenandh_machine_test/domain/core/mainFailures.dart';
 import 'package:sreenandh_machine_test/domain/upadate_product/model/updated_product_model.dart';
 
 
-
+@injectable
 abstract class IUpdateProduct {
   Future<Either<MainFailures, UpdateProduct>> getUpdatedProduct({
     required int productID,
